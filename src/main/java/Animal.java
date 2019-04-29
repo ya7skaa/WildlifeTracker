3af2573ad179105223e6cc7f6348685dcff4a081
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-29 09:25:24 +0300

    method to override equals method created in animal

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,9 +2,11 @@ public class Animal {
     public String name;
     public String endangered;
 
+
     public Animal(String name,String endangered){
         this.name = name;
         this.endangered = endangered;
+
 
     }
 
@@ -15,5 +17,18 @@ public class Animal {
     public String getEndangered(){
         return endangered;
     }
+
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName()) &&
+                    this.getEndangered().equals(newAnimal.getEndangered());
+        }
+    }
+
+
 
 }
