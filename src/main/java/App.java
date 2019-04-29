06cc6c01fe0df14a72fe42f6e80dd2b52af29f16
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-04-30 01:12:12 +0300

    created app.css file for styling my various pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,28 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+
 public class App {
+
+
+    public static void main(String[] args){
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("animals", Animals.all());
+            model.put("endangeredAnimals", EndangeredAnimal.all());
+            model.put("sightings", Sighting.all());
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+    }
 }
