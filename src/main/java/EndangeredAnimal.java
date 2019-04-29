f1c02aec88bc840cc048d8fe068566ea9641106a
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-29 10:13:52 +0300

    test to instantiate endangered animal if its true passed

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,2 +1,7 @@
 public class EndangeredAnimal {
+    public String name;
+
+    public EndangeredAnimal(){
+
+    }
 }
--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EndangeredAnimalTest {
+    @Test
+    public void endangeredAnimal_instantiatesCorrectly_true() {
+        EndangeredAnimal testEndangeredAnimal= new EndangeredAnimal();
+        assertEquals(true, testEndangeredAnimal instanceof EndangeredAnimal);
+
+    }
+
 }
