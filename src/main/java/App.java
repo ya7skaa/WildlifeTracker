b1afe68810331ee9492eecf7329089a843a46fcc
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-30 16:05:22 +0300

    file to add a form to record a new animal details

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -35,7 +35,7 @@ public class App {
 
         //route for adding an endangeredAnimal sighting
 
-        post("/endangeredAnimals", (request, response) -> {
+        post("/endangered_sighting", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             String location = request.queryParams("location");
             String ranger = request.queryParams("ranger");
@@ -50,6 +50,32 @@ public class App {
             model.put("template", "templates/success.vtl");
             return new ModelAndView(model, layout);
         },new VelocityTemplateEngine());
+
+
+        // route for adding non-endangered animal sighting
+        post("/sighting ", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String location = request.queryParams("location");
+            String ranger = request.queryParams("ranger");
+            int animalId = Integer.parseInt(request.queryParams("endangeredAnimalSelected"));
+            Sighting sighting = new Sighting(ranger, location, animalId);
+            sighting.save();
+            model.put("sighting", sighting);
+            model.put("animals", EndangeredAnimal.all());
+
+            String animal = EndangeredAnimal.find(animalId).getName();
+            model.put("animal", animal);
+            model.put("template", "templates/success.vtl");
+            return new ModelAndView(model, layout);
+        },new VelocityTemplateEngine());
+
+
+// route when clicking on "Add Animal to System"
+        get("/animal/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/animal-form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
 
 //        post("/endangered_sighting", (request, response) -> {
