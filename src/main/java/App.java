98bc183b92b3bfdbf01ffaa4d910faf598846e31
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-30 14:27:27 +0300

    created route to a path sighting.vtl when user clicks post sighting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
@@ -5,8 +6,6 @@ import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-
-
 public class App {
 
 
@@ -24,5 +23,16 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        //route when user clicks "Post Sighting"
+        get("/sighting", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("animals", Animals.all());
+            model.put("endangeredAnimals", EndangeredAnimal.all());
+            model.put("template", "templates/sighting.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
+
+
 }
