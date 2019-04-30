0992ced3b7b0dc7b2e751baac8ceba37ab580c20
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-30 15:28:58 +0300

    added forms to record endangered and non endangered animal; sighting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -32,6 +32,28 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+
+        //route for adding an endangeredAnimal sighting
+        post("/endangered_sighting", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            int animalId = Integer.parseInt(request.queryParams("endangeredAnimalSelected"));
+            String health = request.queryParams("health");
+            String age = request.queryParams("age");
+            String location = request.queryParams("location");
+            String ranger = request.queryParams("ranger");
+            Sighting sighting = new Sighting(ranger, location, animalId);
+            sighting.save();
+            // model.put("sighting", sighting);
+            // model.put("animals", EndangeredAnimal.all());
+            String animal = EndangeredAnimal.find(animalId).getName();
+            int selectAnimalId = Integer.parseInt(request.queryParams("endangeredAnimalSelected"));
+            EndangeredAnimal updatedAnimal = EndangeredAnimal.find(animalId);
+            updatedAnimal.update(health, age);
+            model.put("animal", animal);
+            model.put("template", "templates/success.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 
 
